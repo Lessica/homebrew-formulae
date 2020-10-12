2b0def937504b32342d4c63b3d29fd287f12de37
i_82 <5410705+Lessica@users.noreply.github.com>
2020-10-12 17:46:11 +0800

    Update brew-pkg.rb

--- brew-pkg.rb
+++ brew-pkg.rb
@@ -1,7 +1,8 @@
 class BrewPkg < Formula
   desc "Homebrew command for building OS X packages from installed formulae."
   homepage "https://github.com/Lessica/brew-pkg"
-  url "https://github.com/Lessica/brew-pkg.git", :revision => "645b2a6aa5d823edcc698af86413efb7013c3c13"
+  url "https://github.com/Lessica/brew-pkg.git", :tag => "v0.1.7",
+                                                 :revision => "645b2a6aa5d823edcc698af86413efb7013c3c13"
 
   # This is an .rb that must be executable in order for Homebrew to
   # find it with the 'which' method, so we skip_clean
